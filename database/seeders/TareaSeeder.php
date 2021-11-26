21a14b7a9689bd0093516c2293c8bc75bd6811cd
AdriánDW32 <agarciago20dw@ikzubirimanteo.com>
2021-11-26 12:49:32 +0100

    Modificando seeder de tareas para asignarle un usuario_id

--- database/seeders/TareaSeeder.php
+++ database/seeders/TareaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Tarea;
+use App\Models\Usuario;
 
 class TareaSeeder extends Seeder
 {
@@ -14,24 +15,29 @@ class TareaSeeder extends Seeder
      */
     public function run()
     {
-        $tarea = new Tarea;
-        $tarea->nombre = "Estudiar Laravel";
-        $tarea->save();
+        $tarea1 = new Tarea;
+        $tarea1->nombre = "Estudiar Laravel";
+        $tarea1->usuario_id = Usuario::find(1)->id;
+        $tarea1->save();
 
         $tarea2 = new Tarea;
         $tarea2->nombre = "Documentar tarea Despliegues";
+        $tarea2->usuario_id = Usuario::find(2)->id;
         $tarea2->save();
 
         $tarea3 = new Tarea;
         $tarea3->nombre = "Practicar Ajax";
+        $tarea3->usuario_id = Usuario::find(3)->id;
         $tarea3->save();
 
         $tarea4 = new Tarea;
         $tarea4->nombre = "Terminar ejercicio Bootstrap";
+        $tarea4->usuario_id = Usuario::find(1)->id;
         $tarea4->save();
 
         $tarea5 = new Tarea;
         $tarea5->nombre = "Descansar... zzz";
+        $tarea5->usuario_id = Usuario::find(2)->id;
         $tarea5->save();
     }
 }
